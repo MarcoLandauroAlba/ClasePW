d1a73860f79662e8859b7ee0cd22d1390dabc850
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-02-21 23:04:30 -0500

    s4 c3

---
+++ components/banner.component.js
@@ -0,0 +1,18 @@
+const Banner = () => {
+  return (
+    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+
+      <div id="carrusel" className="carousel-inner"></div>
+
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  )
+}
+export default Banner
---
+++ components/footer.component.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="card mt-4">
+      <div className="card-body">
+        Universidad de Lima 2022-0
+      </div>
+    </footer>
+  )
+}
+export default Footer
